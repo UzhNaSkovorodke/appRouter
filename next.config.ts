18dacc2d58fa20ed2240033ca6679ae01abcde9a
UzhNaSkovorodke <tukalov.kirill99@gmail.com>
2024-11-21 00:45:26 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule: any) =>
+            rule.test?.test?.('.svg'),
+        )
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: {not: [...fileLoaderRule.resourceQuery.not, /url/]},
+                use: ['@svgr/webpack'],
+            },
+        )
+        fileLoaderRule.exclude = /\.svg$/i
+        return config
+    },
+
+    images: {
+        remotePatterns: [
+            {hostname: 'cms.stonehedgecompany.com',},
+            {hostname: 'stonehedgecompany.com'},
+            {hostname: 'stonehedgecompany.comnull'},
+            {hostname: 'stonehedgecompany.comundefined'},
+            {hostname: 'cms.stonehedgecompany.comundefined'},
+            {hostname: 'stone.runull'},
+            {hostname: 'stone.ruundefined'},
+            {hostname: '166ba6ce-eee2-43f1-87a4-b80ece869977.selcdn.net'},
+            {hostname: 'demo.stone.ru'},
+            {hostname: 'cms.demo.stone.ru'},
+        ],
+    },
+
 };
 
 export default nextConfig;
